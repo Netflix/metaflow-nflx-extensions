1547dda5d2222c88d821feadad1402ea2f74fddc
Romain Cledat <rcledat@netflix.com>
2023-01-26 09:49:36 -0800

    Update for 2.7.20 compatibility

--- metaflow_extensions/netflix_ext/plugins/mfextinit_netflixext.py
+++ metaflow_extensions/netflix_ext/plugins/mfextinit_netflixext.py
@@ -1,14 +1,9 @@
-from . import conda
-from .conda.conda_flow_decorator import CondaFlowDecorator
-from .conda.conda_step_decorator import CondaStepDecorator
-from .conda.conda_environment import CondaEnvironment
+CLIS_DESC = [("environment", ".environment_cli.cli")]
 
-FLOW_DECORATORS = [CondaFlowDecorator]
-STEP_DECORATORS = [CondaStepDecorator]
-ENVIRONMENTS = [CondaEnvironment]
-
-def get_plugin_cli():
-    from . import environment_cli
-    return [environment_cli.cli]
+FLOW_DECORATORS_DESC = [
+    ("conda_base", ".conda.conda_flow_decorator.CondaFlowDecorator")
+]
+STEP_DECORATORS_DESC = [("conda", ".conda.conda_step_decorator.CondaStepDecorator")]
+ENVIRONMENTS_DESC = [("conda", ".conda.conda_environment.CondaEnvironment")]
 
 __mf_promote_submodules__ = ["conda"]
