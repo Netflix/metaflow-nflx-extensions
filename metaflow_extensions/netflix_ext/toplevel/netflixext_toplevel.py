6c9b1732e13d1cc73a99c58a6cb7e3c4b04b8026
Romain Cledat <rcledat@netflix.com>
2024-12-06 12:11:24 -0800

    Add import of parsers at top level

--- metaflow_extensions/netflix_ext/toplevel/netflixext_toplevel.py
+++ metaflow_extensions/netflix_ext/toplevel/netflixext_toplevel.py
@@ -1,4 +1,7 @@
 from .netflixext_version import netflixext_version
 
+# Parsers
+from ..plugins.conda.parsers import req_parser, yml_parser
+
 __mf_extensions__ = "netflix-ext"
 __version__ = netflixext_version
