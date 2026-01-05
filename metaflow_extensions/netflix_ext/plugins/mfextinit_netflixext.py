b6315fe431fd9ba176e45c7cd92d06fe38cc3fbb
Romain Cledat <rcledat@netflix.com>
2026-01-05 01:35:22 -0800

    Added missing imports

--- metaflow_extensions/netflix_ext/plugins/mfextinit_netflixext.py
+++ metaflow_extensions/netflix_ext/plugins/mfextinit_netflixext.py
@@ -5,12 +5,24 @@ FLOW_DECORATORS_DESC = [
     ("pip_base", ".conda.conda_flow_decorator.PipRequirementFlowDecorator"),
     ("pypi_base", ".conda.conda_flow_decorator.PypiRequirementFlowDecorator"),
     ("named_env_base", ".conda.conda_flow_decorator.NamedEnvRequirementFlowDecorator"),
+    (
+        "sys_packages_base",
+        ".conda.conda_flow_decorator.SysPackagesRequirementFlowDecorator",
+    ),
 ]
 STEP_DECORATORS_DESC = [
     ("conda", ".conda.conda_step_decorator.CondaRequirementStepDecorator"),
     ("pip", ".conda.conda_step_decorator.PipRequirementStepDecorator"),
     ("pypi", ".conda.conda_step_decorator.PypiRequirementStepDecorator"),
+    (
+        "pylock_toml_internal",
+        ".conda.conda_step_decorator.PylockTomlInternalDecorator",
+    ),
     ("named_env", ".conda.conda_step_decorator.NamedEnvRequirementStepDecorator"),
+    (
+        "sys_packages",
+        ".conda.conda_step_decorator.SysPackagesRequirementStepDecorator",
+    ),
     ("conda_env_internal", ".conda.conda_step_decorator.CondaEnvInternalDecorator"),
 ]
 ENVIRONMENTS_DESC = [("conda", ".conda.conda_environment.CondaEnvironment")]
--- metaflow_extensions/netflix_ext/toplevel/netflixext_toplevel.py
+++ metaflow_extensions/netflix_ext/toplevel/netflixext_toplevel.py
@@ -1,4 +1,6 @@
 from .netflixext_version import netflixext_version
 
+from ..plugins.conda.conda_flow_mutator import ResolvedUVEnvFlowDecorator as resolved_uv
+
 __mf_extensions__ = "netflix-ext"
 __version__ = netflixext_version
