3c9b09495821a293942470a4da026252639c323b
Nissan Pow <npow@netflix.com>
2026-03-05 19:26:38 -0800

    Fix integration test: pass required location kwarg to create_table

--- metaflow-fastdata/tests/test_hive_table.py
+++ metaflow-fastdata/tests/test_hive_table.py
@@ -344,6 +344,7 @@ class TestHiveThriftCatalogUnit:
 class TestHiveThriftCatalogIntegration:
     TEST_DB = "default"
     TEST_TABLE = "mf_fastdata_test_hive"
+    TEST_LOCATION = "s3://metaflow-fastdata-test/default/mf_fastdata_test_hive"
 
     def test_create_and_read_table(self, hive_catalog):
         info = hive_catalog.create_table(
@@ -352,6 +353,7 @@ class TestHiveThriftCatalogIntegration:
             catalog_name="hive",
             column_schema=[("id", "int"), ("name", "string")],
             partition_schema=[("dt", "string")],
+            location=self.TEST_LOCATION,
         )
         assert info.db == self.TEST_DB
         assert info.table == self.TEST_TABLE
