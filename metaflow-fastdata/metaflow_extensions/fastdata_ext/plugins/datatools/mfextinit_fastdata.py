f4a984217d494ee0c7ac6b56b86569956ece97c1
Nissan Pow <npow@netflix.com>
2026-03-05 19:00:58 -0800

    Move plugins/mfextinit_fastdata.py to plugins/ top-level; fix imports

--- metaflow-fastdata/metaflow_extensions/fastdata_ext/plugins/datatools/mfextinit_fastdata.py
+++
@@ -1,4 +0,0 @@
-from .dataframe import MetaflowDataFrame
-from .cache import Cache, CacheError
-
-__mf_promote_submodules__ = ["dataframe", "md", "ops", "iceberg", "cache"]
---
+++ metaflow-fastdata/metaflow_extensions/fastdata_ext/plugins/mfextinit_fastdata.py
@@ -0,0 +1,4 @@
+from .datatools.dataframe import MetaflowDataFrame
+from .datatools.cache import Cache, CacheError
+
+__mf_promote_submodules__ = ["datatools"]
