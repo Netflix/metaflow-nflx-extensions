22f36d8d589c10285972039b106fcbadd265585a
berg-davidj <berg.davidj@gmail.com>
2026-09-23 15:39:49 -0700

    Update keywords.py

--- metaflow-functions/metaflow_extensions/nflx/plugins/functions/backends/keywords.py
+++ metaflow-functions/metaflow_extensions/nflx/plugins/functions/backends/keywords.py
@@ -1,4 +1,2 @@
-# Kwargs that address the backend rather than the decorated function, so that
-# `f(data, process=1)` does not hand `process` to the user's code. Shared so
-# the backends cannot disagree about which names are theirs.
+# These kwargs are reserved for the runtime not the user code 
 KEYWORDS = {"process", "params"}
